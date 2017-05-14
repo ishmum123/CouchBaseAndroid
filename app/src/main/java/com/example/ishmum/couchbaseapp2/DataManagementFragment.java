91bc35fa1bf07fa1ce93989f8266c2fae86c6fa7
ishmum <ishmumkhan@yahoo.co.in>
2017-05-14 13:29:33 +0600

    First Commit

---
+++ app/src/main/java/com/example/ishmum/couchbaseapp2/DataManagementFragment.java
@@ -0,0 +1,18 @@
+package com.example.ishmum.couchbaseapp2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DataManagementFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_data_management, container, false);
+        return v;
+    }
+
+}
